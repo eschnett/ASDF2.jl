fb759d8f5996b4355f0c51821721740cbbae096c
Documenter.jl <documenter@juliadocs.github.io>
2023-11-18 02:14:16 +0000

    build based on 5cd1fc3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ASDF2.jl","page":"ASDF2.jl","title":"ASDF2.jl","text":"","category":"section"},{"location":"","page":"ASDF2.jl","title":"ASDF2.jl","text":"Modules = [IndexSpaces]","category":"page"}]
+}
